f2347e8758dcd11447be194d4b5b1c3f612c068e
EsparzaRSE <esparzarse@gmail.com>
2023-11-11 09:48:45 +0100

    Ejercicio 17 - Triangle con enum

--- triangle/triangle.cpp
+++ triangle/triangle.cpp
@@ -2,15 +2,15 @@
 
 namespace triangle {
 
-    bool kind(float one, float two, float three){
+    flavor kind(float one, float two, float three){
 
         if(((one < 0) || (two < 0) || (three < 0)) 
         || ((one == 0) && (two == 0) && (three == 0)) 
         || ((one + two <= three) || (one + three <= two) || (two + three <= one))) throw std::domain_error("Illegal");
 
-        if((one == two) && (two == three)) return triangle::flavor::equilateral;
-        else if((one == two) || (one == three) || (two == three)) return triangle::flavor::isosceles;
-        else if((one != two) && (two != three)) return true;
-        else return false;
+        if((one == two) && (two == three)) return flavor::equilateral;
+        else if((one == two) || (one == three) || (two == three)) return flavor::isosceles;
+        else if((one != two) && (two != three)) return flavor::scalene;
+        else return flavor::fail;
     }
 }  // namespace triangle
--- triangle/triangle.h
+++ triangle/triangle.h
@@ -5,13 +5,11 @@
 
 namespace triangle {
 
-    struct flavor{ //struct ya que todo es público
-        static const bool equilateral{true};
-        static const bool isosceles{true};
-        static const bool scalene{true};
+    enum class flavor{ //struct ya que todo es público
+        equilateral, isosceles, scalene, fail,
     };
 
-    bool kind(float one, float two, float three); //float ya que los flotantes van a ser pequeños, no veo necesario un double
+    flavor kind(float one, float two, float three); //float ya que los flotantes van a ser pequeños, no veo necesario un double
 }  // namespace triangle
 
 #endif // TRIANGLE_H
