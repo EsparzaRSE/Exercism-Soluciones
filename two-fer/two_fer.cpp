93468131f1593a980b855842792bed41eca62918
EsparzaRSE <esparzarse@gmail.com>
2023-11-13 13:50:44 +0100

    Ejercicio 21 - Two fer referencia y eliminado if

--- two-fer/two_fer.cpp
+++ two-fer/two_fer.cpp
@@ -2,9 +2,8 @@
 
 namespace two_fer
 {
-    std::string two_fer(std::string nombre){
-        if(nombre.empty()) return "One for you, one for me.";
-        else return "One for " + nombre + ", one for me.";
+    std::string two_fer(const std::string& nombre){
+        return "One for " + nombre + ", one for me.";
     }
 } // namespace two_fer
 
