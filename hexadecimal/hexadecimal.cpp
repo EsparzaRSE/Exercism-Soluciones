ace491273aa5d2ebee1ab94ae821d78a414b404f
EsparzaRSE <esparzarse@gmail.com>
2023-11-24 20:34:07 +0100

    Ejercicio 41 - Hexadecimal mejorado

--- hexadecimal/hexadecimal.cpp
+++ hexadecimal/hexadecimal.cpp
@@ -2,19 +2,12 @@
 
 namespace hexadecimal {
     int convert(std::string_view str_hexa){
-
         int decimal{0};
-        int posicion{0};
 
         for(int i{static_cast<int>(str_hexa.size()) - 1}; i >= 0; --i){
-            int digito{0};
-            
-            if(isdigit(str_hexa[i])) digito = str_hexa[i] - '0';
-            else if(str_hexa[i] >= 'a' && str_hexa[i] <= 'f') digito = str_hexa[i] - 'W';
+            if(isdigit(str_hexa[i])) decimal += (str_hexa[i] - '0') * pow(16, str_hexa.size() - 1 - i);
+            else if(str_hexa[i] >= 'a' && str_hexa[i] <= 'f') decimal += (str_hexa[i] - 'W') * pow(16, str_hexa.size() - 1 - i);
             else return 0;
-            
-            decimal += digito*(pow(16,posicion));
-            ++posicion;          
         }
         return decimal;
     }
